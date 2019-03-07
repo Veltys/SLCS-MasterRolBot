b96e7d3479201e4971e2371dab63ac29bad52d75
Veltys <veltys@veltys.es>
2019-03-07 22:02:24 +0100

    ADD: Carga del archivo del token

--- main.py
+++ main.py
@@ -7,16 +7,41 @@
 # Author        : jesusFx
 # Author        : Veltys
 # Date          : 07-03-2019
-# Version       : 0.0.0
+# Version       : 0.0.1
 # Usage         : python3 main.py
 # Notes         : 
+
+
+DEBUG           = False                                                                             # Flag de depuración
 
 
 import sys                                                                                          # Funcionalidades varias del sistema
 
 
+def cargar_token():
+    try:
+        archivo_token_bot = open('.bot.token', 'r')
+
+        token_bot = archivo_token_bot.read()
+    
+        archivo_token_bot.close()
+        
+        return token_bot
+
+    except FileNotFoundError:
+        return None
+
+
 def main(argv):
-    print('Hola mundo')
+    token_bot = cargar_token()
+
+    if(token_bot != None):
+        print('El token para este bot es ' + token_bot)
+
+        # TODO: Seguir aquí
+
+    else:
+        print('Error: No se ha encontrado el archivo con el token para este bot')
 
 
 if __name__ == '__main__':
