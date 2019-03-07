43139cbe3d0d12d68e48da827575f55080c283c2
Veltys <veltys@veltys.es>
2019-03-07 21:35:51 +0100

    ADD: main.py ➡ Hola Mundo

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Title         : main.py
+# Description   : Función main del bot
+# Author        : jesusFx
+# Author        : Veltys
+# Date          : 07-03-2019
+# Version       : 0.0.0
+# Usage         : python3 main.py
+# Notes         : 
+
+
+import sys                                                                                          # Funcionalidades varias del sistema
+
+
+def main(argv):
+    print('Hola mundo')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
