d5a42feccefaeffe1c2f824f69721f590c585c66
Veltys <veltys@veltys.es>
2019-04-16 17:26:34 +0200

    ADD: Más datos de prueba

--- BB. DD..sql
+++ BB. DD..sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS "Opciones";
 CREATE TABLE IF NOT EXISTS "Opciones" (
 	"Id"	INTEGER NOT NULL,
 	"Estado"	INTEGER NOT NULL,
-	"Nombre"	TEXT NOT NULL,
-	"Seguiente"	INTEGER NOT NULL,
+	"Siguiente"	INTEGER NOT NULL,
 	PRIMARY KEY("Id"),
-	FOREIGN KEY("Estado") REFERENCES "Estados"("Id")
+	FOREIGN KEY("Estado") REFERENCES "Estados"("Id"),
+	FOREIGN KEY("Siguiente") REFERENCES "Estados"("Id")
 );
 DROP TABLE IF EXISTS "Juegos";
 CREATE TABLE IF NOT EXISTS "Juegos" (
@@ -28,10 +28,18 @@ CREATE TABLE IF NOT EXISTS "Usuarios" (
 	PRIMARY KEY("Id"),
 	FOREIGN KEY("Estado") REFERENCES "Estados"("Id")
 );
+INSERT INTO "Opciones" VALUES (1,1,2);
+INSERT INTO "Opciones" VALUES (2,1,3);
+INSERT INTO "Opciones" VALUES (3,1,4);
+INSERT INTO "Opciones" VALUES (4,1,5);
 INSERT INTO "Juegos" VALUES (0,'Sin juego elegido');
 INSERT INTO "Juegos" VALUES (1,'El rol de Cálico Electrónico');
 INSERT INTO "Estados" VALUES (0,0,'Sin juego elegido','Estado "especial" que aglutina a los usuarios que no han elegido ningún juego');
 INSERT INTO "Estados" VALUES (1,1,'¡Bienvenido!','Nuestro superhéroe favorito ha sido secuestrado por un villano desconocido. Por si eso no fuera suficiente, el equipo de héroes que estaba dedicado a buscarle, los Huérfanos Electrónicos... ¡tampoco aparecen!
 
 ¡¡ES TU MOMENTO DE ENTRAR EN ACCIÓN!!');
+INSERT INTO "Estados" VALUES (2,1,'Investigas la base secreta','La puerta está cerrada, vas a tener que buscar la manera de entrar.');
+INSERT INTO "Estados" VALUES (3,1,'Investigas la casa de Cálico Jack','Llamas al timbre de la casa y... ¡es la madre de Cálico! Te ofrece unos _boquerones en su tinta_, un _bocaillo shope_, un _bocata foigrá_ y unas _cocretas_. ¿Qué elegirás?');
+INSERT INTO "Estados" VALUES (4,1,'Investigas Electrónica web','Nada, que no se abre la puerta. ¿Eso es un felpudo con la cara de Muzaman?');
+INSERT INTO "Estados" VALUES (5,1,'Visita _ar chacho Migué_','¿Investigar? ¡Eso es para déblies! Vamos a visitar _ar chacho Migué_, que seguro que sabe algo o al menos nos venderá algún arma.');
 COMMIT;
